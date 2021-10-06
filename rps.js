9472ccf4c137d88c069dcf9d33f5ee06b882c219
Anthony Scott <anthony.scott@cousinssubs.com>
2021-10-06 15:20:45 -0500

    Updates to follow the rules a little more closely

--- rps.js
+++ rps.js
@@ -1,16 +1,31 @@
-let compResult;
-let compHand;
-let playerHand;
+let scoreAlert;
+let roundAlert;
 let nonLoseResult;
 let loseResult;
 let win;
+let compRandom;
+let compHand;
+let playerHand;
 let startAgain;
+let playerScore;
+let compScore;
+let roundCount;
 
-function compSelect() {  //* Randomly get a computer selection of either Rock, Paper, or Scissors
-	compResult = Math.floor(Math.random() * 3 +1);
-	if (compResult === 1) { 
+alert('Rock, Paper, Scissors JS console game.  rps() to begin.')
+
+function update() { //* Update round variables for ease
+	scoreAlert = 'The score is ' + playerScore + ' to ' + compScore + '.';
+	roundAlert = 'Preparing for round number ' + roundCount + '.';
+	nonLoseResult = 'You chose ' + playerHand + ' and the computer chose ' + compHand + '.';
+	loseResult = 'The computer chose ' + compHand + ' and you chose ' + playerHand + '.';
+	win = null;
+}
+
+function compSelect() { //* Randomly get a computer selection of either Rock, Paper, or Scissors
+	compRandom = Math.floor(Math.random() * 3 + 1);
+	if (compRandom === 1) { 
 		compHand = "rock";
-	} else if (compResult === 2) {
+	} else if (compRandom === 2) {
 		compHand = 'scissors';
 	} else {
 		compHand = 'paper';
@@ -35,16 +50,21 @@ function playAgain() { //* Asks if you would like to start a new game
 	} else if (startAgain === 'no') {
 		alert ('Well then, until next time, good sir!');
 	} else {
-		playGame();
+		playerScore = 0;
+		compScore = 0;
+		roundCount = 1;
+		rps();
 	}
 }
 
-function playGame() { //* Compares computer to player choice, gives a win/lose message
+function playGame() {  //* Compares computer to player choice, gives a win/lose message
+	update();
+	alert(scoreAlert);
+	alert(roundAlert);
+	alert('Computer has made their choice.')
 	compSelect();
 	playerSelect();
-	nonLoseResult = 'You chose ' + playerHand + ' and the computer chose ' + compHand + '.'; 
-	loseResult = 'The computer chose ' + compHand + ' and you chose ' + playerHand + '.';
-	win = null;
+	update();
 	if (compHand === playerHand) {
 		alert(nonLoseResult);
 		alert('It\'s a tie!');
@@ -75,13 +95,31 @@ function playGame() { //* Compares computer to player choice, gives a win/lose m
 	if (win === true) {
 		alert(nonLoseResult);
 		alert('You win!!!');
-		playAgain();
+		playerScore++;
+		roundCount++;
 	} else if (win === false) {
 		alert(loseResult);
-		alert('You lose... :(');
-		playAgain();
-	}
-	if (compHand === playerHand) {
+		alert('You lose...  :(');
+		compScore++;
+		roundCount++;
+	} else {
 		playGame();
+	}
+}
+
+function rps() {
+	playerScore = 0;
+	compScore = 0;
+	roundCount = 1;
+	for (let i = 0; i < 5; i++){
+		playGame();
+	}
+	if(playerScore > compScore) {
+		alert('Congratulations!  You won ' + playerScore + ' out of ' + (roundCount - 1) + ' games!');
+		playAgain();
+	} else {
+		alert('Too bad, your opponent has bested you once again.');
+		alert('You lost by ' + (compScore - playerScore) + '!');
+		playAgain();
 	}
 }	
